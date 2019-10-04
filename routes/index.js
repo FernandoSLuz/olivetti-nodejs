362eb4a422528f97e4f36adf593f7bb6d571c31b
Jairo Iglesias (SPARK) <jairo.iglesias@sparkconsultoria.com>
2019-10-04 17:12:29 -0300

    testando gcloud deploy

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,8 @@ let express = require('express');
 let router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/node-express', function(req, res, next) {
+  res.render('index', { title: 'Express Teste 123' });
 });
 
 router.post('/webhook', (req, res) => {
