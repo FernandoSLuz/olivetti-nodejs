546095e547540dc7dc627190823b2871fdb0d3b5
Jairo Iglesias (SPARK) <jairo.iglesias@sparkconsultoria.com>
2019-10-05 04:01:25 -0300

    First Commit

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,9 @@
 let express = require('express');
 let router = express.Router();
+let rp = require('request-promise');
+
+const URL_ROOT = 'https://www.selfscanner.net/wsbackend/users/hackathon/'
+const APIKEY_SELFSCANNER = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyTmFtZSI6ImhhY2thdGhvbiIsInVzZXJUeXBlIjoicmVndWxhciIsImlhdCI6MTU2OTMzNDk0Mn0.wf6JYu6zt0gCxNPMPRWFae9vvlZrj9eaRAgXJIDP3kM'
 
 /* GET home page. */
 router.get('/node-express', function(req, res, next) {
@@ -15,5 +19,52 @@ router.post('/webhook', (req, res) => {
   })
 
 })
+
+router.get('/intent_user/:intent_type', async (req, res) => {
+
+  const intentType = req.params.intent_type.toLowerCase()
+  let response = ''
+
+  switch(intentType){
+    case 'products':
+      response = await getAllProducts()
+      break
+    default:
+      response = {status: 'not found'}
+  }
+
+  res.status(200).send(response)
+
+})
+
+
+
+const getAllProducts = async () => {
+
+  var options = {
+    uri: 'https://www.selfscanner.net/wsbackend/users/hackathon/products',
+    headers: {
+      'Authorization' : `Bearer ${APIKEY_SELFSCANNER}`
+    },
+    resolveWithFullResponse: true
+  }
+
+  let response = await rp(options)
+
+  return response.body
+
+}
+
+
+// ### TESTE ###
+
+// (async () => {
+  
+//   const resp = await getAllProducts()
+//   console.log(resp)
+
+// })()
+
+
 
 module.exports = router;
